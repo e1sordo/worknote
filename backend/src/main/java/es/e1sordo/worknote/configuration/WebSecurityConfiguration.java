f6c0678a5349a1719e38d505cdaa95d3c6c5cab4
e1sordo <vlavik007@gmail.com>
2023-07-05 17:39:55 +0300

    update dockerfile

---
+++ backend/src/main/java/es/e1sordo/worknote/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package es.e1sordo.worknote.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
